0d75af935a4aba8920b7ce73b004a7fd76dc5025
Michael Klishin <mklishin@gopivotal.com>
2014-03-22 00:41:59 +0400

    Fix attributes, this cookbook no longer provisions 2 Leiningen versions

--- leiningen/attributes/default.rb
+++ leiningen/attributes/default.rb
@@ -1,2 +1,2 @@
-default[:leiningen][:lein2][:version]        = "2.3.4"
-default[:leiningen][:lein2][:install_script] = "https://github.com/technomancy/leiningen/raw/#{leiningen[:lein2][:version]}/bin/lein"
+default[:leiningen][:version]        = "2.3.4"
+default[:leiningen][:install_script] = "https://github.com/technomancy/leiningen/raw/#{leiningen[:lein2][:version]}/bin/lein"
