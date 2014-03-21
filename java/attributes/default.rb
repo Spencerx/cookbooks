27cf479224edcdda4d94820564ae5f015ca8b0d1
Michael Klishin <mklishin@gopivotal.com>
2014-03-22 00:54:21 +0400

    Provision openjdk7 by default
    Oracle policy file downloads fail, needs
    investigation.

--- java/attributes/default.rb
+++ java/attributes/default.rb
@@ -3,7 +3,7 @@ default['java']['arch'] = arch
 
 default['java']['jvm_base_dir']                        = '/usr/lib/jvm'
 
-default['java']['default_version']                     = 'oraclejdk7'
+default['java']['default_version']                     = 'openjdk7'
 default['java']['openjdk7']['jvm_name']                = "java-1.7.0-openjdk-#{node['java']['arch']}"
 default['java']['oraclejdk7']['jvm_name']              = 'java-7-oracle'
 default['java']['oraclejdk7']['install_jce_unlimited'] = true
