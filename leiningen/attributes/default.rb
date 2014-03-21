d78492f7bfa57633400c6c3b550d935cf19b806d
Michael Klishin <mklishin@gopivotal.com>
2014-03-22 00:43:56 +0400

    More attribute fixing

--- leiningen/attributes/default.rb
+++ leiningen/attributes/default.rb
@@ -1,2 +1,6 @@
-default[:leiningen][:version]        = "2.3.4"
-default[:leiningen][:install_script] = "https://github.com/technomancy/leiningen/raw/#{leiningen[:lein2][:version]}/bin/lein"
+vsn = "2.3.4"
+
+default[:leiningen] = {
+  :version        => vsn
+  :install_script => "https://github.com/technomancy/leiningen/raw/#{vsn}/bin/lein"
+}
