9fabf862112546da0198f2fec59e56c81d18c044
Michael Klishin <mklishin@gopivotal.com>
2014-04-01 01:26:12 +0400

    ssh_keys cookbook: initial cut

---
+++ ssh_keys/metadata.rb
@@ -0,0 +1,5 @@
+name             "ssh_keys"
+maintainer       "OpenSensors Development Team"
+license          "Apache v2.0"
+description      "Provisions private and public SSH keys for users"
+
---
+++ ssh_keys/recipes/default.rb
@@ -0,0 +1,52 @@
+#
+# Cookbook Name:: ssh_keys
+# Recipe:: default
+#
+# Author:: Michael Klishin (<michael@opensensors.io>)
+#
+# Copyright 2014, OpenSensors Development Team <team@opensensors.io>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+node.fetch(:private_ssh_keys, {}).each do |user, bag|
+  # e.g. {"dir"=>"/home/vagrant", "gid"=>1000, "uid"=>1000, "shell"=>"/bin/bash", "gecos"=>""}
+  user_info   = node.etc.passwd[user]
+  item        = data_bag_item("ssh_keys", bag)
+  ssh_dir     = File.join(user_info.dir, ".ssh")
+
+  directory ssh_dir do
+    owner  user_info.uid
+    group  user_info.gid || user_info.uid
+    mode   "0700"
+    action :create
+  end
+
+  template File.join(ssh_dir, "id_rsa") do
+    source "content.erb"
+    variables(content: item['private_key'])
+
+    owner  user_info.uid
+    group  user_info.gid || user_info.uid
+    mode   "0600"
+  end
+
+  template File.join(ssh_dir, "id_rsa.pub") do
+    source "content.erb"
+    variables(content: item['public_key'])
+
+    owner  user_info.uid
+    group  user_info.gid || user_info.uid
+    mode   "0600"
+  end
+end
