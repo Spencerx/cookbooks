11a134802015bf3bfafcb5e8715c7e96e1f17647
Michael Klishin <mklishin@gopivotal.com>
2014-04-01 01:54:36 +0400

    Add metadata

---
+++ azondi/metadata.rb
@@ -0,0 +1,13 @@
+maintainer        "OpenSensors Team"
+maintainer_email  "michael@opensensors.io"
+license           "Apache 2.0"
+description       "Provisions Azondi, part of the OpenSensors project"
+version           "2.0.0"
+
+supports 'ubuntu', '>= 12.04'
+
+%w{ git leiningen }.each do |cb|
+  depends cb
+end
+
+recipe "azondi::default", "Provisions Azondi from source"
