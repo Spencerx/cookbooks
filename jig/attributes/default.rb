edeb6deeb5875c11a98febbd737980d801ac8370
Michael Klishin <mklishin@gopivotal.com>
2014-04-03 01:52:15 +0400

    Use full sha hash

--- jig/attributes/default.rb
+++ jig/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:jig] = {
   clone_url: "git://github.com/juxt/jig.git",
-  revision: "03f40a5",
+  revision: "03f40a55465920e6627df510b48edbe42444616a",
   user: "vagrant",
   included_configs: []
 }
